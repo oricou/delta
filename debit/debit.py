f7111d91184a6ab2c366447a154b26b30beddd0f
philippe.bernet <philippe.bernet@epita.fr>
2022-03-16 20:21:29 +0100

    feat : nationale

--- debit/debit.py
+++ debit/debit.py
@@ -1,5 +1,5 @@
 import sys
-import urllib.request 
+import urllib.request
 import dash
 import flask
 import json
@@ -12,134 +12,168 @@ import plotly.express as px
 import os.path
 import folium
 
+
+class Echelle():
+    def __init__(self, data, geojson, id, short):
+        self.data = data
+        self.geojson = geojson
+        self.id = id
+        self.short = short
+
+
 class HautDebit():
     START = 'Start'
-    STOP  = 'Stop'
+    STOP = 'Stop'
 
     # Data
     urls = ['https://www.data.gouv.fr/fr/datasets/r/d538685a-b9cb-4a3e-b90d-ad6f0a13920b',
             'https://raw.githubusercontent.com/gregoiredavid/france-geojson/master/regions-avec-outre-mer.geojson',
-            'https://raw.githubusercontent.com/gregoiredavid/france-geojson/master/departements-avec-outre-mer.geojson']
+            'https://raw.githubusercontent.com/gregoiredavid/france-geojson/master/departements-version-simplifiee.geojson',
+            'https://raw.githubusercontent.com/gregoiredavid/france-geojson/master/metropole-et-outre-mer.geojson']
 
     paths = ['./data/2021t4-obs-hd-thd-deploiement.xlsx',
              'data/regions.geojson',
-             'data/departements.geojson']
+             'data/departements.geojson',
+             'data/france.geojson']
 
     def download_data(self):
-        for i in range (len(self.paths)):
+        for i in range(len(self.paths)):
             path, url = self.paths[i], self.urls[i]
             if not os.path.isfile(path):
                 print(f"Downloading {path} from {url}.")
                 urllib.request.urlretrieve(url, path)
 
+    def init_nationale(self):
+        df = pd.read_excel(self.paths[0],
+                           sheet_name='Régions',
+                           header=4,
+                           usecols="B,F,G:W")
+
+        df = df.sum(axis=0)
+        df[1:].astype(int, copy = False)
+        df = pd.DataFrame(df).T
+        df.rename(columns={'Nom région' : 'Nom'}, inplace=True)
+        df['Nom'] = 'France'
+
+        geojson = json.load(open(self.paths[3]))
+        geojson["properties"] = {"nom": "France"}
+        geojson = {
+            "type": "FeatureCollection",
+            "features": [
+                geojson,
+            ]
+        }
+
+        return Echelle(df, geojson, 'Nom', 'Nat.')
+
     def init_region(self):
-        self.data_region = pd.read_excel(self.paths[0],
-                                    sheet_name='Régions',
-                                    header=4,
-                                    usecols="B,F:W")
-        self.geo_region = json.load(open(self.paths[1]))
+        return Echelle(pd.read_excel(self.paths[0],
+                                     sheet_name='Régions',
+                                     header=4,
+                                     usecols="B,F,G:W"),
+                       json.load(open(self.paths[1])),
+                       'Nom région',
+                       'Rég.')
 
     def init_departement(self):
-        self.data_departement = pd.read_excel(self.paths[0],
-                                    sheet_name='Départements',
-                                    header=4,
-                                    usecols="B,G:X")
-        self.geo_departement = json.load(open(self.paths[2]))
+        return Echelle(pd.read_excel(self.paths[0],
+                                     sheet_name='Départements',
+                                     header=4,
+                                     usecols="B,G,H:X"),
+                       json.load(open(self.paths[2])),
+                       'Nom département',
+                       'Dép')
 
-    # def init_commune(self):
-    #     self.data_commune = pd.read_excel(self.paths[0],
-    #                                 sheet_name='Communes',
-    #                                 header=4,
-    #                                 usecols="B,K,R:AH")
-        
-
-    def __init__(self, application = None):
-        self.data_region, self.geo_region = None, None
-        self.data_departement, self.geo_departement = None, None
-        # self.data_commune = None
-              
+    def __init__(self, application=None):
         self.download_data()
 
-        self.init_region()
-        self.init_departement()
-        # self.init_commune()
+        self.region = self.init_region()
+        self.departement = self.init_departement()
+        self.nationale = self.init_nationale()
 
-        self.year = self.data_region.columns[2:]
+        self.echelles = [self.nationale, self.region, self.departement]
+
+        self.year = self.region.data.columns[2:]
 
         self.main_layout = html.Div(children=[
             html.H3(children='Acces au haut débit en France'),
 
             html.Div([
-                    html.Div([ dcc.Graph(id='debit-main-map'), ], style={'width':'90%', }),
+                html.Div([dcc.Graph(id='debit-main-map'), ],
+                         style={'width': '90%', }),
 
-                    html.Div([
-                        html.Div('Échelle géographique'),
-                        dcc.RadioItems(
-                            id='debit-crossfilter-scale-type',
-                            options=[{'label': i, 'value': i} for i in ['Rég.', 'Dép.']], # , 'Commune']],
-                            value='Rég.',
-                            labelStyle={'display':'block'},
-                        ),
-                        html.Br(),
-                        html.Br(),
-                        html.Div('Type'),
-                        dcc.RadioItems(
-                            id='debit-crossfilter-type-type',
-                            options=[{'label': i, 'value': i} for i in ['Taux', 'Brute']],
-                            value='Taux',
-                            labelStyle={'display':'block'},
-                        ),
-                        html.Br(),
-                        html.Br(),
-                        html.Button(
-                            self.START,
-                            id='debit-button-start-stop', 
-                            style={'display':'inline-block'}
-                        ),
-                    ], style={'margin-left':'20px', 'width': '7em', 'float':'right'}),
-                ], style={
-                    'padding': '10px 50px', 
-                    'display':'flex',
-                    'justifyContent':'center'
-                }),            
-            
+                html.Div([
+                    html.Div('Échelle géographique'),
+                    dcc.RadioItems(
+                        id='debit-crossfilter-scale-type',
+                        options=[{'label': i.short, 'value': i.short}
+                                 for i in self.echelles],
+                        value=self.echelles[0].short,
+                        labelStyle={'display': 'block'},
+                    ),
+                    html.Br(),
+                    html.Br(),
+                    html.Div('Type'),
+                    dcc.RadioItems(
+                        id='debit-crossfilter-type-type',
+                        options=[{'label': i, 'value': i}
+                                 for i in ['Taux', 'Brute']],
+                        value='Taux',
+                        labelStyle={'display': 'block'},
+                    ),
+                    html.Br(),
+                    html.Br(),
+                    html.Button(
+                        self.START,
+                        id='debit-button-start-stop',
+                        style={'display': 'inline-block'}
+                    ),
+                ], style={'margin-left': '20px', 'width': '7em', 'float': 'right'}),
+            ], style={
+                'padding': '10px 50px',
+                'display': 'flex',
+                'justifyContent': 'center'
+            }),
+
             html.Div([
                 html.Div(
                     dcc.Slider(
-                            id='debit-crossfilter-year-slider',
-                            min=0,
-                            max=len(self.year) - 1,
-                            step = 1,
-                            value=0,
-                            marks={i: self.year[i] for i in range(len(self.year))},
+                        id='debit-crossfilter-year-slider',
+                        min=0,
+                        max=len(self.year) - 1,
+                        step=1,
+                        value=0,
+                        marks={i: self.year[i]
+                               for i in range(len(self.year))},
                     ),
-                    style={'display':'inline-block', 'width':"90%"}
+                    style={'display': 'inline-block', 'width': "90%"}
                 ),
                 dcc.Interval(            # fire a callback periodically
                     id='debit-auto-stepper',
                     interval=1500,       # in milliseconds
-                    max_intervals = -1,  # start running
-                    n_intervals = 0
+                    max_intervals=-1,  # start running
+                    n_intervals=0
                 ),
-                ], style={
-                    'padding': '0px 50px', 
-                    'width':'100%'
-                }),
+            ], style={
+                'padding': '0px 50px',
+                'width': '100%'
+            }),
 
             html.Br(),
             dcc.Markdown("""
-            Le graphique est interactif. En passant la souris sur les régions/départements, vous avez une infobulle. 
-            
+            Le graphique est interactif. En passant la souris sur les régions/départements, vous avez une infobulle.
+            A l'echelle nationale et régionale, vous pouvez zoomer/dézoomer pour voir les outres-mer.
+
             Sources : https://www.data.gouv.fr/fr/datasets/le-marche-du-haut-et-tres-haut-debit-fixe-deploiements/
             """)
 
 
         ], style={
-                #'backgroundColor': 'rgb(240, 240, 240)',
-                 'padding': '10px 50px 10px 50px',
-                 }
+            # 'backgroundColor': 'rgb(240, 240, 240)',
+            'padding': '10px 50px 10px 50px',
+        }
         )
-        
+
         if application:
             self.app = application
             # application should have its own layout and use self.main_layout as a page or in a component
@@ -151,9 +185,9 @@ class HautDebit():
         # # (somhow it is more clear to have here all interaction between functions and components)
         self.app.callback(
             dash.dependencies.Output('debit-main-map', 'figure'),
-            [ dash.dependencies.Input('debit-crossfilter-scale-type', 'value'),
-              dash.dependencies.Input('debit-crossfilter-type-type', 'value'),
-              dash.dependencies.Input('debit-crossfilter-year-slider', 'value')])(self.update_map)
+            [dash.dependencies.Input('debit-crossfilter-scale-type', 'value'),
+             dash.dependencies.Input('debit-crossfilter-type-type', 'value'),
+             dash.dependencies.Input('debit-crossfilter-year-slider', 'value')])(self.update_map)
         self.app.callback(
             dash.dependencies.Output('debit-button-start-stop', 'children'),
             dash.dependencies.Input('debit-button-start-stop', 'n_clicks'),
@@ -170,34 +204,32 @@ class HautDebit():
              dash.dependencies.State('debit-button-start-stop', 'children')])(self.on_interval)
 
     def update_map(self, scale, type, year):
-        def get_good_date(scale):
-            if scale == 'Rég.':
-                return self.data_region.copy(), self.geo_region, 'Nom région'
-            elif scale == 'Dép.':
-                return self.data_departement.copy(), self.geo_departement, 'Nom département'
-            # else:
-            #     return self.data_commune.copy(), json.load(open('data/communes-version-simplifiee.geojson')), 'Nom commune'
+        def get_good_scale(scale):
+            for echelle in self.echelles:
+                if echelle.short == scale:
+                    return echelle
+            return None
 
-        data, goejson, keyDF = get_good_date(scale)
-        data = pd.DataFrame(data)
+        echelle = get_good_scale(scale)
+        data, goejson, keyDF = echelle.data.copy(), echelle.geojson, echelle.id
 
-        if type == 'Taux' :
-            data[data.columns[2:]] = data[data.columns[2:]].div(data[data.columns[1]], axis=0)
+        if type == 'Taux':
+            data[data.columns[2:]] = data[data.columns[2:]].div(
+                data[data.columns[1]], axis=0)
             range_col = (0, 1)
         else:
             range_col = (0, data[data.columns[2:]].max().max())
 
+        fig = px.choropleth_mapbox(data, geojson=goejson,
+                                   locations=keyDF, featureidkey='properties.nom',
+                                   color=self.year[year], color_continuous_scale="Viridis",
+                                   mapbox_style="carto-positron",
+                                   zoom=4.2, center={"lat": 47, "lon": 2},
+                                   range_color=range_col,
+                                   opacity=0.5,
+                                   labels={self.year[year]: type})
+        fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
 
-        fig = px.choropleth_mapbox(data, geojson=goejson, 
-                           locations=keyDF, featureidkey = 'properties.nom',
-                           color=self.year[year], color_continuous_scale="Viridis",
-                           mapbox_style="carto-positron",
-                           zoom=4.2, center = {"lat": 47, "lon": 2},
-                           range_color=range_col,
-                           opacity=0.5,
-                           labels={self.year[year]: type})
-        fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
-        
         return fig
 
     def get_country(self, hoverData):
@@ -219,7 +251,7 @@ class HautDebit():
     # in the same callback
     def run_movie(self, text):
         if text == self.START:    # then it means we are stopped
-            return 0 
+            return 0
         else:
             return -1
 
