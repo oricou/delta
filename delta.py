d1f58d2fd628ae65ecd8a451e6d53f7bbfa69d96
Eliott Bouhana <eliott.bouhana@epita.fr>
2022-03-18 14:42:06 +0100

    add vscode settings
    Signed-off-by: Eliott Bouhana <eliott.bouhana@epita.fr>

--- delta.py
+++ delta.py
@@ -341,6 +341,65 @@ def init():
 
 app = init()
 server = app.server
+pop = population.WorldPopulationStats(app)
+nrg = energies.Energies(app)
+dec = deces.Deces(app)
+
+main_layout = html.Div([
+    html.Div(className = "row",
+             children=[ 
+                 dcc.Location(id='url', refresh=False),
+                 html.Div(className="two columns",
+                          children = [
+                              html.Center(html.H2("Δelta δata")),
+                              dcc.Link(html.Button("Prix d'énergies", style={'width':"100%"}), href='/energies'),
+                              html.Br(),
+                              dcc.Link(html.Button('Natalité vs revenus', style={'width':"100%"}), href='/population'),
+                              html.Br(),
+                              dcc.Link(html.Button('Décès journaliers', style={'width':"100%"}), href='/deces'),
+                              html.Br(),
+                              html.Br(),
+                              html.Br(),
+                              html.Center(html.A('Code source', href='https://github.com/eliottness/delta')),
+                          ]),
+                 html.Div(id='page_content', className="ten columns"),
+            ]),
+])
+
+
+home_page = html.Div([
+    html.Br(),
+    html.Br(),
+    html.Br(),
+    html.Br(),
+    dcc.Markdown("Choisissez le jeu de données dans l'index à gauche."),
+])
+
+to_be_done_page = html.Div([
+    dcc.Markdown("404 -- Désolé cette page n'est pas disponible."),
+])
+
+app.layout = main_layout
+
+# "complete" layout (not sure that I need that)
+app.validation_layout = html.Div([
+    main_layout,
+    to_be_done_page,
+    pop.main_layout,
+])
+
+# Update the index
+@app.callback(dash.dependencies.Output('page_content', 'children'),
+              [dash.dependencies.Input('url', 'pathname')])
+def display_page(pathname):
+    if pathname == '/energies':
+        return nrg.main_layout
+    elif pathname == '/population':
+        return pop.main_layout
+    elif pathname == '/deces':
+        return dec.main_layout
+    else:
+        return home_page
 
 if __name__ == '__main__':
     profile = False
