207cc10f74cac104ddcd67f7da3779059f4ec70e
Rokkano <pierre.de-la-ruffie@epita.fr>
2022-03-24 11:58:06 +0100

    feat: added utils file with (for now) Union and Intersection mathematical functions

---
+++ pdlr-mg_sujet/utils.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+# <---------------------- MATH UTILS ---------------------->
+
+def Union(list1, list2):
+    return list(set(list1) | set(list2))
+
+def Intersection(list1, list2):
+    return list(set(list1) & set(list2))
