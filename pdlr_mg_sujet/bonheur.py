e20108ab9c129c1288b297a1a858e6288e0d1528
Rokkano <pierre.de-la-ruffie@epita.fr>
2022-03-24 17:44:30 +0100

    feat: added a bunch of things

--- pdlr_mg_sujet/bonheur.py
+++ pdlr_mg_sujet/bonheur.py
@@ -15,18 +15,43 @@ import dateutil as du
 # empecher des erreurs peu importe le chemin de la source
 sys.path.insert(0, os.path.dirname(os.path.realpath(__file__)))
 from get_data import df_hr, df_cc
+from utils import *
+
 
 class Bonheur():
     
-    
+    continent_list_en = ["Africa", "Asia", "Europe", "North America", "Oceania", "South America"] 
+    continent_list_fr = ["Afrique", "Asie", "Europe", "Amérique du Nord", "Océanie", "Amérique du Sud"]
     
     def __init__(self, application = None):
-        self.df = df_hr
         
-
-        
-        self.main_layout = html.Div()
-
+        self.main_layout = html.Div([
+            html.H3("Étude du bonheur mondial de 2005 à 2020"),
+            dcc.Markdown(md_introduction),
+            html.Div([
+                dcc.Graph(
+                    id='bnh-primary-graph', 
+                    config={'displayModeBar': False},
+                )
+            ], style = {'width' : '100%'}),
+            html.Div([
+                dcc.RadioItems(
+                    id='bnh-continent',
+                     options=[
+                         {'label':'Afrique', 'value':'Africa'}, 
+                         {'label':'Amérique du Nord','value':'North America'}, 
+                         {'label':'Amérique du Sud','value':'South America'}, 
+                         {'label':'Asie','value':'Asia'}, 
+                         {'label':'Europe','value':'Europe'}, 
+                         {'label':'Océanie','value':'Oceania'},
+                    ],
+                    labelStyle={'display':'block'},
+                )
+            ])
+        ], style = {
+            'backgroundColor': 'white',
+            'padding': '10px 50px 10px 50px',
+        })
         if application:
             self.app = application
         else:
@@ -34,6 +59,42 @@ class Bonheur():
             self.app.layout = self.main_layout
 
 
+        self.app.callback(
+            dash.dependencies.Output('bnh-primary-graph', 'figure'),
+            [
+                dash.dependencies.Input('bnh-continent', 'value')
+            ])(self.update_graph)
+            
+            
+    def update_graph(self, _, param = "Echelle de vie"):
+        values = pd.pivot_table(df_hr, values="Echelle de vie", columns="Continent", aggfunc=np.mean).values.tolist()[0]
+        fig = px.bar(
+            x = self.continent_list_fr,
+            y = values,
+            labels=dict(x = param, y = "Continents"),
+            template='plotly_white'
+        )
+        return fig
+
+    
 if __name__ == '__main__':
     nrg = Bonheur()
     nrg.app.run_server(debug=True, port=8051)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+# <------------------------------------------------------->
+md_introduction = """
+Gallup Organization est une entreprise américaine offrant de nombreux services de recherche concernant le managment, les ressources humaines et les statistiques. Cette entité est particulièrement connue pour efféctuer différents sondages souvent médiatisés.
+"""
+
+
--- pdlr_mg_sujet/get_data.py
+++ pdlr_mg_sujet/get_data.py
@@ -8,20 +8,6 @@ path_hapiness_report = abs_path + "data/Happiness-Report.csv"
 path_countries_continents = abs_path + "data/Countries-Continents.csv"
 
 
-# <----------------------IMPORT ET TRAITEMENT DU DATASET Happiness-Report---------------------->
-
-df_hr = pd.read_csv(path_hapiness_report, sep=",", encoding="UTF-8")
-
-# Renommage des colonnes
-df_hr_col_en = ["Country name","year","Life Ladder","Log GDP per capita",
-              "Social support","Healthy life expectancy at birth","Freedom to make life choices",
-              "Generosity","Perceptions of corruption,Positive affect","Negative affect"]
-df_hr_col_fr = ["Pays","Année","Echelle de vie","log PIB par habitant","Support social",
-              "Espérance de vie","Liberté de vivre","Générosité","Perception de la corruption",
-              "Effets positifs","Effets négatifs"]
-df_hr.columns = df_hr_col_fr
-## df_hr.interpolate(method = 'linear', inplace = True)
-
 # <--------------------IMPORT ET TRAITEMENT DU DATASET Countries-Continents-------------------->
 
 df_cc = pd.read_csv(path_countries_continents, sep=",", encoding="UTF-8")
@@ -31,3 +17,23 @@ df_cc_col_en = ["Continents", "Countries"]
 df_cc_col_fr = ["Continent", "Pays"]
 df_cc.columns = df_cc_col_fr
 df_cc.set_index("Pays", inplace = True)
+
+
+# <----------------------IMPORT ET TRAITEMENT DU DATASET Happiness-Report---------------------->
+
+df_hr = pd.read_csv(path_hapiness_report, sep=",", encoding="UTF-8")
+# Renommage des colonnes
+df_hr_col_en = ["Country name", "year","Life Ladder","Log GDP per capita",
+              "Social support","Healthy life expectancy at birth","Freedom to make life choices",
+              "Generosity","Perceptions of corruption,Positive affect","Negative affect"]
+df_hr_col_fr = ["Pays", "Année", "Echelle de vie","log PIB par habitant","Support social",
+              "Espérance de vie","Liberté de vivre","Générosité","Perception de la corruption",
+              "Effets positifs","Effets négatifs"]
+df_hr.columns = df_hr_col_fr
+
+# Ajoute la colonne des continent et la déplace en 2ème position
+df_hr = df_hr.join(df_cc, on = "Pays")
+tmp = df_hr.pop("Continent")
+df_hr.insert(1, 'Continent', tmp)
+
+## df_hr.interpolate(method = 'linear', inplace = True)
