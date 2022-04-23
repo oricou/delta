92294a5295da89309e02a608b9a3907abef6c92a
Guillaume Carriere <guillaume.carriere@epita.fr>
2022-04-23 05:07:56 +0200

    add scatterboxmap

--- chloro/chloro.py
+++ chloro/chloro.py
@@ -1,5 +1,6 @@
 import sys
 import glob
+from tkinter import W
 import dash
 import flask
 from dash import dcc
@@ -12,12 +13,76 @@ import dateutil as du
 from scipy import stats
 from scipy import fft
 import datetime
+import xarray as xr
+
+import numpy as np
 
 class Chloro():
-    def __init__(self, application = None):
+    def create_fig(self):
+        mapbox_access_token = 'pk.eyJ1IjoiZ2NhcnJpZXJlIiwiYSI6ImNsMmI1c3p3ejAxNmEzaW51MXBta2N6bTcifQ.f_MlUBEyToUp92xcCOwF0g'
 
+        data = []
+
+        data.append(
+            go.Scattermapbox(
+                lon=self.gdmpdf['lonbin'].values,
+                lat=self.gdmpdf['latbin'].values,
+                mode='markers',
+                text=["GDMP" + '<br>' + str(self.gdmpdf['GDMP'].iloc[i]) for i in range(self.gdmpdf.shape[0])],
+                marker=go.Marker(
+                    cmax=330,
+                    cmin=0,
+                    color=self.gdmpdf['GDMP'].values,
+                    colorscale='viridis'
+                ),
+            )
+        )
+        data.append(
+            go.Scattermapbox(
+                lon=self.chldf['lonbin'].values,
+                lat=self.chldf['latbin'].values,
+                mode='markers',
+                text=["Chlorophylle" + '<br>' + str(self.chldf['chl'].iloc[i]) for i in range(self.chldf.shape[0])],
+                marker=go.Marker(
+                    cmax=2.5,
+                    cmin=-2.5,
+                    color=self.chldf['chl'].values,
+                    colorscale='viridis',
+                ),
+            )
+        )
+ 
+        layout = go.Layout(
+            margin=dict(t=0,b=0,r=0,l=0),
+            autosize=True,
+            hovermode='closest',
+            showlegend=False,
+            mapbox=dict(
+                accesstoken=mapbox_access_token,
+                bearing=0,
+                center=dict(
+                    lat=38,
+                    lon=-94
+                ),
+                pitch=0,
+                zoom=0,
+                style='dark'
+            ),
+        )
+
+        fig = dict(data=data, layout=layout)
+        return fig
+
+
+
+    def __init__(self, application = None):
+        
+        self.chldf = pd.read_pickle("data/chldf.pkl")
+        self.gdmpdf = pd.read_pickle("data/gdmpdf.pkl")
+        fig = self.create_fig()
         self.main_layout = html.Div(children=[
             html.H3(children='Chlorophylle x GDMP'),
+            html.Div([ dcc.Graph(figure=fig)], style={'width':'100%', }),
             html.Br(),
             dcc.Markdown("""
             Lorem Ipsum blablabla
@@ -34,6 +99,7 @@ class Chloro():
         else:
             self.app = dash.Dash(__name__)
             self.app.layout = self.main_layout
+
 
 
         
