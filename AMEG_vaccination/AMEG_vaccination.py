00924e11ffde71a25e4bbb2dd7a785b512f07264
Erwan <wan.goudard@gmail.com>
2022-04-29 11:36:52 +0200

    [EDIT]: add PIB to AMEG_vaccination.py

--- AMEG_vaccination/AMEG_vaccination.py
+++ AMEG_vaccination/AMEG_vaccination.py
@@ -19,12 +19,21 @@ def load_data(filename):
     return df
 
 
+def load_PIB(filename):
+    df = pd.read_csv(filename)
+    df = df[df["Year"] == 2016]
+    df = df.drop(["Country Name", "Year"], axis=1)
+    df = df.rename(columns={"Country Code": "iso_code", "Value": "PIB"})
+    return df
+
+
 class Vaccinations():
 
     def __init__(self, application=None):
         vacc = load_data("data/vaccinations.csv")
+        pib = load_PIB("data/gdp.csv")
 
-        self.vacc = vacc
+        self.vacc = vacc.merge(pib, how="left", on="iso_code")
 
         self.main_layout = html.Div(children=[
             html.H3(children='Vaccination contre le COVID-19 par pays et par date'),
