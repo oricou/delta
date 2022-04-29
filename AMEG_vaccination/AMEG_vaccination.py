5185db12d92c4134f034de83d02c184844c9e7c4
Adrien Merat <adrien.merat@epita.fr>
2022-04-29 12:07:56 +0200

    [EDIT] Continue `AMEG_vaccination.py`

--- AMEG_vaccination/AMEG_vaccination.py
+++ AMEG_vaccination/AMEG_vaccination.py
@@ -19,19 +19,34 @@ def load_data(filename):
     return df
 
 
-class Vaccinations():
+class Vaccinations:
 
     def __init__(self, application=None):
         vacc = load_data("data/vaccinations.csv")
 
         self.vacc = vacc
+        self.cols = [
+            'Vaccinations totales',
+            'Personnes vaccinées',
+            'Personnes totalement vaccinées',
+            'Total de boosters',
+            'Vaccinations quotidiennes brutes',
+            'Vaccinations quotidiennes nettes',
+            'Vaccinations pour 100 habitants',
+            'Personnes vaccinées pour 100 habitants',
+            'Personnes totalement vaccinées pour 100 habitants',
+            'Total de boosters pour 100 habitants',
+            'Vaccinations quotidiennes pour 1M habitants',
+            'Personnes vaccinées quotidiennement',
+            'Personnes vaccinées quotidiennement pour 100 habitants',
+        ]
 
         self.main_layout = html.Div(children=[
             html.H3(children='Vaccination contre le COVID-19 par pays et par date'),
-            html.Div([dcc.Graph(id='ngr-main-graph'), ], style={'width':'100%', }),
+            html.Div([dcc.Graph(id='ngr-main-graph'), ], style={'width': '100%', }),
         ], style={
             'backgroundColor': '#f5f5f5',
-            'padding':'10px 50px 10px 50px',
+            'padding': '10px 50px 10px 50px',
         })
 
         if application:
@@ -40,6 +55,28 @@ class Vaccinations():
             self.app = dash.Dash(__name__)
             self.app.layout = self.main_layout
 
+        self.app.callback(
+            dash.dependencies.Output('ngr-main-graph', 'figure'),
+            dash.dependencies.Input('ngr-main-graph', 'value')
+        )(self.update_graph)
+
+    def update_graph(self, _):
+        df = self.vacc
+
+        fig = px.line(df[df.columns[2]], template='plotly_dark')
+        for c in df.columns[3:]:
+            fig.add_scatter(x=df.index, y=df[c], mode='lines', name=c, text=c, hoverinfo='x+y+text')
+
+        fig.update_layout(
+            title='Vaccinations contre le COVID-19',
+            xaxis=dict(title='Temps'),
+            yaxis=dict(title='Vaccinations'),
+            height=600,
+            showlegend=True,
+        )
+        return fig
+
 
 if __name__ == '__main__':
-    Vaccinations()
+    vacci = Vaccinations()
+    vacci.app.run_server(debug=True, port=8051)
