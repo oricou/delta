47084499b65d6266915558a5990e11e4b48c7d2d
charlie <charlie.brosse@epita.fr>
2022-05-03 19:50:14 +0200

    python files

--- delta.py
+++ delta.py
@@ -1,17 +1,22 @@
+from turtle import rt
 import dash
 from dash import dcc
 from dash import html
-from energies import energies
-from population import population
-from deces import deces
+from producteurs import producteurs
+from keywords_popularite import keywords_popularite
+from evolution_production import evolution_production
+from theme_popularite import theme_popularite
+from critique_budget import critique_budget 
 
 # external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-app = dash.Dash(__name__,  title="Delta", suppress_callback_exceptions=True) # , external_stylesheets=external_stylesheets)
+app = dash.Dash(__name__,  title="Movie industry analysis", suppress_callback_exceptions=True) # , external_stylesheets=external_stylesheets)
 server = app.server
-pop = population.WorldPopulationStats(app)
-nrg = energies.Energies(app)
-dec = deces.Deces(app)
+prd = producteurs.Producer(app)
+kwA = keywords_popularite.KeywordsAnalysis(app)
+mvP = evolution_production.MovieProduction(app)
+rtA = critique_budget.RatingAnalysis(app)
+tmA = theme_popularite.ThemeAnalysis(app)
 
 main_layout = html.Div([
     html.Div(className = "row",
@@ -19,16 +24,20 @@ main_layout = html.Div([
                  dcc.Location(id='url', refresh=False),
                  html.Div(className="two columns",
                           children = [
-                              html.Center(html.H2("Δelta δata")),
-                              dcc.Link(html.Button("Prix d'énergies", style={'width':"100%"}), href='/energies'),
+                              html.Center(html.H2("Movie Data")),
+                              dcc.Link(html.Button("Evolution de la production de films", style={'width':"100%"}), href='/production'),
                               html.Br(),
-                              dcc.Link(html.Button('Natalité vs revenus', style={'width':"100%"}), href='/population'),
+                              dcc.Link(html.Button('Comparaison du succès critique par rapport au budget', style={'width':"100%"}), href='/succescritique'),
                               html.Br(),
-                              dcc.Link(html.Button('Décès journaliers', style={'width':"100%"}), href='/deces'),
+                              dcc.Link(html.Button('Corrélation entre thème et popularité', style={'width':"100%"}), href='/themepopularite'),
+                              html.Br(),
+                              dcc.Link(html.Button('Analyse de l\'impact des producteurs', style={'width':"100%"}), href='/producteurs'),
+                              html.Br(),
+                              dcc.Link(html.Button('Corrélation entre keywords et popularité', style={'width':"100%"}), href='/keywordspopularite'),
                               html.Br(),
                               html.Br(),
                               html.Br(),
-                              html.Center(html.A('Code source', href='https://github.com/oricou/delta')),
+                              html.Center(html.A('Code source', href='https://github.com/CarlitoCepillo/pybd_movie_project')),
                           ]),
                  html.Div(id='page_content', className="ten columns"),
             ]),
@@ -53,19 +62,23 @@ app.layout = main_layout
 app.validation_layout = html.Div([
     main_layout,
     to_be_done_page,
-    pop.main_layout,
+    mvP.main_layout,
 ])
 
 # Update the index
 @app.callback(dash.dependencies.Output('page_content', 'children'),
               [dash.dependencies.Input('url', 'pathname')])
 def display_page(pathname):
-    if pathname == '/energies':
-        return nrg.main_layout
-    elif pathname == '/population':
-        return pop.main_layout
-    elif pathname == '/deces':
-        return dec.main_layout
+    if pathname == '/production':
+        return mvP.main_layout
+    elif pathname == '/succescritique':
+        return rtA.main_layout
+    elif pathname == '/themepopularite':
+        return tmA.main_layout
+    elif pathname == '/producteurs':
+        return prd.main_layout
+    elif pathname == 'keywordspopularite':
+        return kwA.main_layout
     else:
         return home_page
 
---
+++ evolution_production/evolution_production.py
@@ -0,0 +1,102 @@
+import sys
+import dash
+import flask
+from dash import dcc
+from dash import html
+import pandas as pd
+import numpy as np
+import plotly.graph_objs as go
+import plotly.express as px
+import dateutil as du
+
+class MovieProduction():
+
+    def __init__(self, application = None):
+        
+        self.main_layout = html.Div(children=[
+            html.H3(children='Évolution des prix de différentes énergies en France'),
+            html.Div([ dcc.Graph(id='nrg-main-graph'), ], style={'width':'100%', }),
+            html.Div([
+                html.Div([ html.Div('Prix'),
+                           dcc.RadioItems(
+                               id='nrg-price-type',
+                               options=[{'label':'Absolu', 'value':0}, 
+                                        {'label':'Équivalent J','value':1},
+                                        {'label':'Relatif : 1 en ','value':2}],
+                               value=1,
+                               labelStyle={'display':'block'},
+                           )
+                         ], style={'width': '9em'} ),
+                html.Div([ html.Div('Mois ref.'),
+                           dcc.Dropdown(
+                               id='nrg-which-month',
+                               options=[{'label': i, 'value': Energies.mois[i]} for i in Energies.mois],
+                               value=1,
+                               disabled=True,
+                           ),
+                         ], style={'width': '6em', 'padding':'2em 0px 0px 0px'}), # bas D haut G
+                html.Div([ html.Div('Annee ref.'),
+                           dcc.Dropdown(
+                               id='nrg-which-year',
+                               options=[{'label': i, 'value': i} for i in self.years],
+                               value=2000,
+                               disabled=True,
+                           ),
+                         ], style={'width': '6em', 'padding':'2em 0px 0px 0px'} ),
+                html.Div(style={'width':'2em'}),
+                html.Div([ html.Div('Échelle en y'),
+                           dcc.RadioItems(
+                               id='nrg-xaxis-type',
+                               options=[{'label': i, 'value': i} for i in ['Linéaire', 'Logarithmique']],
+                               value='Logarithmique',
+                               labelStyle={'display':'block'},
+                           )
+                         ], style={'width': '15em', 'margin':"0px 0px 0px 40px"} ), # bas D haut G
+                ], style={
+                            'padding': '10px 50px', 
+                            'display':'flex',
+                            'flexDirection':'row',
+                            'justifyContent':'flex-start',
+                        }),
+                html.Br(),
+                dcc.Markdown("""
+                Le graphique est interactif. En passant la souris sur les courbes vous avez une infobulle. 
+                En cliquant ou double-cliquant sur les lignes de la légende, vous choisissez les courbes à afficher.
+                
+                Notes :
+                   * FOD est le fioul domestique.
+                   * Pour les prix relatifs, seules les énergies fossiles sont prises en compte par manque de données pour les autres.
+                #### À propos
+                * Sources : 
+                   * [base Pégase](http://developpement-durable.bsocom.fr/Statistiques/) du ministère du développement durable
+                   * [tarifs réglementés de l'électricité](https://www.data.gouv.fr/en/datasets/historique-des-tarifs-reglementes-de-vente-delectricite-pour-les-consommateurs-residentiels/) sur data.gouv.fr
+                * (c) 2022 Olivier Ricou
+                """)
+        ], style={
+            'backgroundColor': 'white',
+             'padding': '10px 50px 10px 50px',
+             }
+        )
+
+        if application:
+            self.app = application
+            # application should have its own layout and use self.main_layout as a page or in a component
+        else:
+            self.app = dash.Dash(__name__)
+            self.app.layout = self.main_layout
+
+        self.app.callback(
+                    dash.dependencies.Output('nrg-main-graph', 'figure'),
+                    [ dash.dependencies.Input('nrg-price-type', 'value'),
+                      dash.dependencies.Input('nrg-which-month', 'value'),
+                      dash.dependencies.Input('nrg-which-year', 'value'),
+                      dash.dependencies.Input('nrg-xaxis-type', 'value')])(self.update_graph)
+        self.app.callback(
+                    [ dash.dependencies.Output('nrg-which-month', 'disabled'),
+                      dash.dependencies.Output('nrg-which-year', 'disabled')],
+                      dash.dependencies.Input('nrg-price-type', 'value') )(self.disable_month_year)
+        
+if __name__ == '__main__':
+    mvP = MovieProduction()
+    mvP.app.run_server(debug=True, port=8051)
+
