c313b78ef5ca860329517e2cff8d3207c0709463
Rokkano <pierre.de-la-ruffie@epita.fr>
2022-05-04 18:10:40 +0200

    WIP'

--- pdlr_mg_sujet/bonheur.py
+++ pdlr_mg_sujet/bonheur.py
@@ -2,6 +2,7 @@ import sys, os
 import dash, flask
 
 from dash import dcc, html
+import dash_bootstrap_components as dbc
 
 import pandas as pd
 import numpy as np
@@ -48,6 +49,20 @@ class Bonheur():
                     ),
                 ], style = {'width' : '75%', 'display': 'inline-block'}),
                 html.Div([
+                    dbc.Button("Open modal", id="open", n_clicks=0),
+                    dbc.Modal(
+                        [
+                            dbc.ModalHeader(dbc.ModalTitle("Header")),
+                            dbc.ModalBody("This is the content of the modal"),
+                            dbc.ModalFooter(
+                                dbc.Button(
+                                    "Close", id="close", className="ms-auto", n_clicks=0
+                                )
+                            ),
+                        ],
+                        id="modal",
+                        is_open=False,
+                    ),
                     dcc.RadioItems(
                         id = 'bnh-graph-type',
                         options = self.radioItems,
@@ -104,6 +119,17 @@ class Bonheur():
                 dash.dependencies.Input('bnh-btn', 'n_clicks')
             ]
         )(self._reset_graph_values)
+        
+        
+        self.app.callback(
+            Output("modal", "is_open"),
+            [Input("open", "n_clicks"), Input("close", "n_clicks")],
+            [State("modal", "is_open")],
+        )
+        def toggle_modal(n1, n2, is_open):
+            if n1 or n2:
+                return not is_open
+            return is_open
 
     def _reset_graph(self, btn):
         self.mode = 1
