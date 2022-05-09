ace9e181223bc2d151dc72bbabeb457153c4493d
Emmanuel <emmanuel.mollard@epita.fr>
2022-05-09 21:44:10 +0200

    [chart] update correlation

--- chart/chart.py
+++ chart/chart.py
@@ -15,12 +15,8 @@ import json
 class Chart():
 
     def __init__(self, application = None):
-        self.pn_df = pd.read_pickle('data/pn_db.pkl')
-        self.departament = self.pn_df[(self.pn_df['Département'] != 'France_Métro') & (self.pn_df['Département'] != 'France_Entière')]
-        with urlopen('https://france-geojson.gregoiredavid.fr/repo/departements.geojson') as response:
-            self.counties = json.load(response)
-
-        self.years = self.pn_df.index.unique().sort_values()
+        self.ts_df = pd.read_pickle('data/ts_db.pkl')
+        self.all_france = self.ts_df[(self.ts_df['Département'] == 'France_Entière')]
 
         self.main_layout = html.Div(children=[
             html.H3(children='Évolution du nombre de crimes en France.'),
@@ -36,61 +32,15 @@ class Chart():
                 }
             ),
 
-            html.Br(),
-            html.Div([ # slider
-                html.Div(
-                    dcc.Slider(
-                            id='chart-year-slider',
-                            min=0,
-                            max=len(self.years) -1,
-                            value=0,
-                            marks={i: str(self.years[i].year) for i in range(0,len(self.years), 12) },
-                    ),
-                    style={'display':'inline-block', 'width':"90%"}
-                ),
-                dcc.Interval(            # fire a callback periodically
-                    id='chart-auto-stepper',
-                    interval=1000,       # in milliseconds
-                    max_intervals = -1,  # start running
-                    n_intervals = 0,
-                    disabled=True,
-                ),
-                ], style={
-                    'padding': '0px 50px', 
-                    'width':'100%'
-                }),
-
             html.Div([
                 dcc.Checklist(
                     id='chart-items-crimes',
-                    options=[{'label': self.pn_df.columns[i], 'value': i }
-                        for i in range(len(self.pn_df.columns) - 1)],
+                    options=[{'label': self.ts_df.columns[i], 'value': i }
+                        for i in range(len(self.ts_df.columns) - 1)],
                     value=[0, 1, 2, 3],
-                    inline=True
+                    inline=False
                 ),
-                html.Button(
-                    'START',
-                    id='chart-button-start-stop', 
-                    n_clicks=0,
-                    style={'display':'inline-block'}
-                ),
-                html.Div([ html.Div('Choix du type de crime:'),
-                           dcc.Dropdown(
-                               id='chart-which-crime',
-                               options=[{'label': i, 'value': i} for i in self.pn_df.columns[:-1]],
-                               value='Autres délits',
-                               disabled=False,
-                           ),
-                         ], style={'width': '32em', 'margin':"0px 0px 0px 40px"}), # bas D haut G
                 html.Div(style={'width':'2em'}),
-                html.Div([ html.Div('Échelle en y'),
-                           dcc.RadioItems(
-                               id='chart-xaxis-type',
-                               options=[{'label': i, 'value': i != 'Linéaire'} for i in ['Linéaire', 'Logarithmique']],
-                               value=True,
-                               labelStyle={'display':'block'},
-                           )
-                         ], style={'width': '15em', 'margin':"0px 0px 0px 40px"} ), # bas D haut G
                 ], style={
                             'padding': '10px 50px', 
                             'display':'flex',
@@ -129,46 +79,31 @@ class Chart():
 
         self.app.callback(
                 dash.dependencies.Output('chart-heatmap', 'figure'),
-                dash.dependencies.Input('chart-year-slider', 'value'),
+                dash.dependencies.Input('chart-items-crimes', 'value'),
                 )(self.update_heatmap)
 
         self.app.callback(
                 dash.dependencies.Output('chart-evolution', 'figure'),
-                dash.dependencies.Input('chart-year-slider', 'value'),
+                dash.dependencies.Input('chart-items-crimes', 'value'),
                 )(self.update_evolution)
 
-        self.app.callback(
-                dash.dependencies.Output('chart-year-slider', 'value'),
-                dash.dependencies.Input ('chart-auto-stepper', 'n_intervals'),
-                dash.dependencies.State('chart-year-slider', 'value'))(self.on_interval)
-
-        self.app.callback(
-                dash.dependencies.Output('chart-auto-stepper', 'disabled'),
-                dash.dependencies.Output('chart-button-start-stop', 'children'),
-                dash.dependencies.Input('chart-button-start-stop', 'n_clicks'),
-                dash.dependencies.State('chart-auto-stepper', 'disabled'),
-                )(self.on_click)
-
-    def update_heatmap(self, date_index):
-        fig = px.imshow([[1, 20, 30],
-                 [20, 1, 60],
-                 [30, 60, 1]])
+    def update_heatmap(self, items):
+        df = self.all_france[self.ts_df.columns[items]]
+        corr = df.sort_index().corr(method='pearson')
+        fig = px.imshow(
+                corr,
+                zmin = -1,
+                zmax = 1,
+        )
+        fig.update_yaxes(matches=None, visible=False)
+        fig.update_xaxes(matches=None, visible=False)
 
         return fig
 
-    def update_evolution(self, date_index):
+    def update_evolution(self, items):
         fig = px.line(x=["a","b","c"], y=[1,3,2], title="sample figure")
 
         return fig
-
-    def on_interval(self, _, year):
-        if year == self.years[-1]:
-            return self.years[0]
-        else:
-            return year + 1
-
-    def on_click(self, n_clicks, disabled):
-        return (not disabled, ("STOP" if disabled else "START"))
 
 
 if __name__ == '__main__':
