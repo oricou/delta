171636c7d3e61a4b763319057f7a9bcb081844f0
Henri <hjamet@student.42.fr>
2022-05-09 20:42:06 +0200

    [FIX]  Import error in standalone app

--- CDHJ_SCEI/src/app.py
+++ CDHJ_SCEI/src/app.py
@@ -4,6 +4,11 @@ from dash import (
     Dash,
 )
 
+import sys
+import os
+
+sys.path.append(os.path.join(os.path.dirname(__file__), "..", ".."))
+
 from CDHJ_SCEI.src.SCEI_graph import SCEI_graph
 
 flask_app = flask.Flask(__name__)
