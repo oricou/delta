6df2f366b81064b958d7275f5353e142e728e60c
Emmanuel <emmanuel.mollard@epita.fr>
2022-05-09 18:21:10 +0200

    [get_data] add a first version

---
+++ get_data.py
@@ -0,0 +1,46 @@
+import pandas as pd
+import re
+import pathlib
+import requests
+
+def read_excel_sheets(xls_path):
+    """Read all sheets of an Excel workbook and return a single DataFrame"""
+    print(f'Loading {xls_path} into pandas')
+    xl = pd.ExcelFile(xls_path)
+    df = list()
+    for idx, name in enumerate(xl.sheet_names):
+        # print(f'Reading sheet #{idx}: {name}')
+
+        sheet = xl.parse(name,
+                         index_col=[0],
+                         skiprows=[96, 97, 99, 100],
+                         usecols=lambda x : x != 'Index').T
+        sheet.index = sheet.index.map(lambda x: pd.to_datetime(x + '_1', format="_%Y_%m_%d"))
+        sheet['Département'] = name
+
+        df.append(sheet)
+    return pd.concat(df)
+
+if __name__ == "__main__":
+    url = "https://www.data.gouv.fr/api/1/datasets/chiffres-departementaux-mensuels-relatifs-aux-crimes-et-delits-enregistres-par-les-services-de-police-et-de-gendarmerie-depuis-janvier-1996/"
+    crimes_resources = requests.get(url).json()
+
+    prog = re.compile(".*\.xlsx")
+    filenames = {
+                    d['title']:d['url']
+                    for d in crimes_resources['resources']
+                    if prog.match(d['title'])
+                }
+
+
+    dfs = {}
+    for k, v in filenames.items():
+        name_db = f'{v[-7:-5]}_db'
+        if pathlib.Path(f'{name_db}.pkl').is_file():
+            dfs[name_db] = pd.read_pickle(f'{name_db}.pkl')
+        else:
+            if not pathlib.Path(k).is_file():
+                with open(k, 'wb') as file:
+                    file.write(requests.get(v).content)
+            dfs[name_db] = read_excel_sheets(k)
+            dfs[name_db].to_pickle(f'{name_db}.pkl')
