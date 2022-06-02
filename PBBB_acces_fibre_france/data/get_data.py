95fd2c32ddfab12330e16fda0095d12ee6e3edfa
Baptiste `Baptman` Bourdet <baptiste.bourdet@epita.fr>
2022-06-02 17:05:25 +0200

    fix(get_data): get data from any directory should work

--- PBBB_acces_fibre_france/data/get_data.py
+++ PBBB_acces_fibre_france/data/get_data.py
@@ -1,21 +1,20 @@
+#!/usr/bin/env python3
 import urllib.request
 import os
+
+from pathlib import Path
 
 urls = ['https://www.data.gouv.fr/fr/datasets/r/d538685a-b9cb-4a3e-b90d-ad6f0a13920b',
         'https://raw.githubusercontent.com/gregoiredavid/france-geojson/master/departements-version-simplifiee.geojson',
         'https://www.arcep.fr/fileadmin/reprise/dossiers/fibre/liste-gestion-identifiants-prefixe-ligne-fibre-2.xlsx']
 
-dossier_data = "./PBBB_acces_fibre_france/data/"
 paths = ['2021t4-obs-hd-thd-deploiement.xlsx',
         'departements.geojson',
         'operator-id.xlsx']
 
 if __name__ == "__main__":
-    if not os.path.isdir(dossier_data):
-        os.mkdir(dossier_data)
-
     for i in range(len(paths)):
         path, url = paths[i], urls[i]
-        if not os.path.isfile(dossier_data + path):
+        if not os.path.isfile(path):
             print(f"Téléchargement {path} depuis {url}.")
-            urllib.request.urlretrieve(url, dossier_data + path)
+            urllib.request.urlretrieve(url, path)
