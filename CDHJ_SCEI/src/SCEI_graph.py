3ba29b92b688cb6f63a38892da2c58ec2cba1693
lopilo24 <henri.jamet@epita.fr>
2022-06-03 14:46:24 +0200

    [FIX] Removing bootstrap from SCEI-GRAPH

--- CDHJ_SCEI/src/SCEI_graph.py
+++ CDHJ_SCEI/src/SCEI_graph.py
@@ -1,9 +1,6 @@
 import flask
 import dash_bootstrap_components as dbc
-from dash import (
-    Dash,
-    html,
-)
+import dash
 
 from tab_classes_preparatoires.classes_preparatoires import Tab_classes_preparatoires
 from tab_grandes_ecoles.grandes_ecoles import Tab_grandes_ecoles
@@ -16,7 +13,7 @@ class SCEI_graph:
             self.app = dash_app
         else:
             flask_app = flask.Flask(__name__)
-            self.app = Dash(
+            self.app = dash.Dash(
                 "SCEI", server=flask_app, external_stylesheets=[dbc.themes.ZEPHYR]
             )
 
@@ -24,23 +21,40 @@ class SCEI_graph:
         self.tab_grandes_ecoles = Tab_grandes_ecoles(dash_app)
         self.tab_a_propos = Tab_a_propos(dash_app)
 
-        self.main_layout = html.Div(
+        self.main_layout = dash.html.Div(
             [
-                html.H1(
+                dash.html.H1(
                     "Impact du lieu de formation sur les intégrations aux Grandes Ecoles",
                     style={"text-align": "center"},
                 ),
-                dbc.Tabs(
-                    [
-                        dbc.Tab(
-                            self.tab_classes_preparatoires.layout,
+                dash.dcc.Tabs(
+                    id="tabs",
+                    value="tab-classes-preparatoires",
+                    children=[
+                        dash.dcc.Tab(
                             label="Classes préparatoires",
+                            value="tab-classes-preparatoires",
                         ),
-                        dbc.Tab(self.tab_grandes_ecoles.layout, label="Grandes Ecoles"),
-                        dbc.Tab(self.tab_a_propos.layout, label="A propos"),
-                    ]
+                        dash.dcc.Tab(
+                            label="Grandes Ecoles", value="tab-grandes-ecoles"
+                        ),
+                        dash.dcc.Tab(label="A propos", value="tab-a-propos"),
+                    ],
                 ),
+                dash.html.Div(id="tab-content"),
             ]
         )
 
         self.app.layout = self.main_layout
+
+        @self.app.callback(
+            dash.Output("tab-content", "children"),
+            dash.Input("tabs", "value"),
+        )
+        def render_content(tab):
+            if tab == "tab-classes-preparatoires":
+                return self.tab_classes_preparatoires.layout
+            elif tab == "tab-grandes-ecoles":
+                return self.tab_grandes_ecoles.layout
+            elif tab == "tab-a-propos":
+                return self.tab_a_propos.layout
